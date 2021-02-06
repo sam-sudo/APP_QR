bf7bd437a91e2f3bcdca4c2dee09e25f9719d896
ck <samuelhueso19@gmail.com>
2021-02-06 01:13:59 +0100

    preguntas tabs

---
+++ APP_QR/app/src/main/java/com/example/app_qr/Fragments/Fragment2.java
@@ -0,0 +1,24 @@
+package com.example.app_qr.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.app_qr.R;
+
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2,container,false);
+
+    }
+}
--- APP_QR/app/src/main/java/com/example/app_qr/MainActivity.java
+++ APP_QR/app/src/main/java/com/example/app_qr/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.app_qr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+import com.example.app_qr.Adapters.MyPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.pager);
+
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(myPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.req0:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.req1:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.req2:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+
+
 }
