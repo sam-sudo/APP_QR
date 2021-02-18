524f9215f7c02e54ce899df1e30316f56fab6cd5
Adrian Avila <adrianavilatorres@gmail.com>
2021-02-18 17:18:27 +0100

    add de la pantalla final y validacion de campos

--- APP_QR/app/src/main/java/com/example/app_qr/GameOver.java
+++ APP_QR/app/src/main/java/com/example/app_qr/GameOver.java
@@ -1,17 +1,53 @@
 package com.example.app_qr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.app_qr.Fragments.Criptex;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameOver extends AppCompatActivity {
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        // Create a new user with a first and last name
+        Map<String, Object> users = new HashMap<>();
+        users.put("username", Login.nombreStr);
+        users.put("lastname", Login.apellidosStr);
+        users.put("time", "No finalizado");
+        users.put("group", Login.grupoStr);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(users)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("" +
+                                "", "Error adding document", e);
+                    }
+                });
+
     }
 
     @Override
